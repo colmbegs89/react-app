01c2652adb95ab4d1f4353a28ecf58dea6188b04
Colm <colm@colms-mbp.derrycitycouncil.org>
2018-01-08 16:20:03 +0000

    tweaks

--- README.md
+++ README.md
@@ -91,28 +91,3 @@ For Production, all the frontend code will be compiled and moved into a static d
 
 Endpoints <--- Express Server ---> Web App
 
-## Project Structure
-TBD
-
-## TODO
-- Should we put some of the common used react libraries into `root/frontend`?
-- Need to discuss the details about how to stucture express code.
-
-
-## Contributing
-
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
-
-## Versioning
-
-We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
-
-## Authors
-
-* **Haochuan Liu** - *Initial work* - [_haochuan](https://haochuan.io)
-
-## License
-
-This project is licensed under the MIT License.
-
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import derrylogo from './dcsdc.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header col-md-12">
+          <div className='row col-md-12'>
+          <img src={derrylogo} className="Council-logo col-md-3" alt="logo" />
+          <img src={logo} className="App-logo col-md-4 offset-md-5" alt="logo" />
+          </div>
+          <div className='row col-md-12'>
           <h1 className="App-title">Welcome to React</h1>
+          </div>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Basic React Application <code>src/App.js</code>.
         </p>
       </div>
     );
